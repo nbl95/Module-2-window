5fde6b6e1ed7d60ee38e888c3ec9c6d45861a6e3
nbl95 <nbl.ltu12@gmail.com>
2020-01-11 05:44:28 +0700

    abc

---
+++ src/FindMaxArray2.java
@@ -0,0 +1,44 @@
+public class FindMaxArray2 {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12}
+        };
+        System.out.println("Max element is :"+FindMax(arr));
+        System.out.println("Min element is :"+ FindMin(arr));
+
+    }
+
+    public static int FindMax(int[][] arr) {
+        int max = arr[0][0];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] > max) {
+                    max = arr[i][j];
+
+                }
+
+            }
+        }
+        return max;
+    }
+
+
+    public static int FindMin(int[][] arr){
+        int min = arr[0][0];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] < min) {
+                    min = arr[i][j];
+
+                }
+
+            }
+        }
+        return min;
+
+    }
+
+
+}

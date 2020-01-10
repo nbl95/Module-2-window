8d86fdcc2c23e96d90290dfbbe224d5c2afc0f2e
nbl95 <nbl.ltu12@gmail.com>
2020-01-10 10:30:42 +0700

    update

--- src/QuadraticEquation.java
+++ src/QuadraticEquation.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
 
 public class QuadraticEquation {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter a:");
+        double a = scanner.nextDouble();
+        System.out.println("enter b:");
+        double b = scanner.nextDouble();
+        System.out.println("enter c:");
+        double c = scanner.nextDouble();
 
+        QuadraticEquation quadraticEquation = new QuadraticEquation(a, b, c);
+        double delta = quadraticEquation.getDiscriminant();
+        if (delta == 0) {
+            System.out.println("Your Root is" + quadraticEquation.getRoot1());
+        } else if (delta > 0) {
+            System.out.println("You have too Root:\n Root 1:" + quadraticEquation.getRoot1() + "\n Root2: " + quadraticEquation.getRoot2());
+        } else {
+            System.out.println("the equation has no roots");
+        }
+    }
+    
     protected double a, b, c;
 
     public QuadraticEquation() {
@@ -30,10 +50,11 @@ public class QuadraticEquation {
         return Math.pow(this.b, 2) - (4 * this.a * this.c);
     }
 
-    public double getRoot1(){
-        return (-this.b + Math.sqrt(Math.pow(this.b,2)-(4*this.a*this.c)))/(2*this.a);
+    public double getRoot1() {
+        return (-this.b + Math.sqrt(Math.pow(this.b, 2) - (4 * this.a * this.c))) / (2 * this.a);
     }
-    public double getRoot2(){
-        return (-this.b - Math.sqrt(Math.pow(this.b,2)-(4*this.a*this.c)))/(2*this.a);
+
+    public double getRoot2() {
+        return (-this.b - Math.sqrt(Math.pow(this.b, 2) - (4 * this.a * this.c))) / (2 * this.a);
     }
 }
